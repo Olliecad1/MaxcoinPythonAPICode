0a94facccb6743f43b54aa7c007697769bad46a2
olliecad1 <officialgamermorris@gmail.com>
2017-09-15 16:18:46 +0000

    new_update

--- getblocknumber.py
+++ getblocknumber.py
@@ -7,12 +7,8 @@ import subprocess
 ## Getting the data
 mining = subprocess.check_output("curl -X GET --header 'Accept: application/json' 'http://api.maxcoinhub.io/Blockchain/GetMiningInfo'", shell=True)
 
-#print mining
-
 ## Converting the string to JSON Format
 new = json.loads(mining)
-
-#print new
 
 ## Getting the latest block
 blocks = new['result']['blocks']
